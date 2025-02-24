728271518cda569ce1d81bcadf2c827c6803ae3b
Javier Quiroga <contact@javierquiroga.com>
2025-02-24 18:57:03 +0000

    Insert prettyPrint method

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -183,4 +183,21 @@ export default class Tree {
 
     return counter;
   }
+
+  prettyPrint(node = this.root, prefix = '', isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? '│   ' : '    '}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
 }
